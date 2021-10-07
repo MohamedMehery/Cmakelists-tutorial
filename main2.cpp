06add17d2165914f5baf4edeab19762daeee3581
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2021-10-07 20:40:38 +0200

    DEV

--- main2.cpp
+++ main2.cpp
@@ -1,17 +1,50 @@
 /*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
 #include <iostream>
 using namespace std;
+
+class vehicle
+{
+    protected:
+        int number_of_tyres;
+    public:
+        void Display(void)
+        {
+            cout << "this is a vehicle" << endl;
+        }
+};
+
+class bike : public vehicle
+{
+    public:
+        void Display(void)
+        {
+            cout << "this is a bike" << endl;
+        }
+        void set_num_of_tyres(int n)
+        {
+            number_of_tyres = n;
+        }
+};
+
+class car : public vehicle
+{
+    public:
+        void Display(void)
+        {
+            cout << "this is a car" << endl;
+        }
+        void set_num_of_tyres(int n)
+        {
+            number_of_tyres = n;
+        }
+};
+
 int main()
 {
 	//Write code here
-	string s ; int fst, snd ;
-	cin >> s ;
-	cin >> fst; 
-	cin >> snd; 
+	unique_ptr<car> cptr1 (new car);
+    cptr1->Display();
 
-	for(int i = fst ; i <= snd ; i++)
-	{
-		cout << s[i] ;
-	}
-	cout << endl;
+
+
 }
