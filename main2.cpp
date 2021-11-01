ba0c2a7b560f5d0621bc60fad27f761d387760b1
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2021-11-01 21:20:47 +0200

    execute cmake . on the cmakelists file

--- main2.cpp
+++ main2.cpp
@@ -33,11 +33,17 @@ class car : public vehicle
     public:
         void Display(void)
         {
-            cout << "\nthis is a car\n" << endl;
+            cout << "\nthis is a car with " << this->number_of_tyres << " tyres. \n" << endl;
         }
         void set_num_of_tyres(int n)
         {
             number_of_tyres = n;
+        }
+
+        car & operator = (const car & c)
+        {
+            this->number_of_tyres = c.number_of_tyres;
+            return *this;
         }
 };
 
@@ -48,6 +54,10 @@ int main()
 
     unique_ptr<vehicle> vptr1( new vehicle);
     car c1;
+    car c2;
+    c2.set_num_of_tyres(5);
+    c1 = c2;
+    c1.Display();
     //vptr1 = move(&c1); //error can't copy unique_ptr
     vptr1->Display();
 
