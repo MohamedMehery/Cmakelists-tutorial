27e25b55d2c688c0e1dbb850b09f680e9aee66d5
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2022-06-24 18:49:34 +0200

    dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
-
-typedef unsigned char u8;
-typedef unsigned short int u16;
+#include <algorithm>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+class Base
+{
+    private:
+        int count;
+    public:
+        Base() : count(0)
+        {
+            cout << "\tContructor!" << endl;
+        }
+        Base(int init) : count(init)
+        {
+            cout << "\tContructor!" << endl;
+        }
+        Base & operator=(Base & b)
+        {
+            cout << "\tAssignment!" <<endl;
+            return *this;
+        }
+        Base( Base & b)
+        {
+            this->count = b.count;
+            cout << "\tCopy contructor!" <<endl;
+        }
+        ~Base()
+        {
+            cout<< "\tDistructor!" << endl;
+        }
+};
+
 int main()
 {
+    Base b1(2) , b2;
+    b2 = b1;
+    Base b3(b1);
 
     return 0;
 }
