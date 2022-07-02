a0554211448374f07bf1117ee250202d826c0a56
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2022-07-02 14:03:02 +0200

    binary search
    add a binsearch.hpp include generic binary search function using (templates and iterators) includes secrets of using iterator with templates

---
+++ src/binsearch.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+typename vector<T>::iterator binsearch( vector<T> &v ,typename vector<T>::iterator start 
+            ,typename vector<T>::iterator last ,const T &elem)
+{
+    if(last>=start)
+    {
+        auto mid = last - (last - start)/2 - v.begin();
+        if(v[mid] == elem) return mid + v.begin() ;
+        if(v[mid] > elem )
+        {
+            last = v.begin() + mid;
+            return binsearch(v , start , last , elem);
+        }
+        else
+        {
+            start = v.begin() + mid;;
+            return binsearch(v , start, last , elem);
+        }
+    }
+    cout <<"\tNot found" <<endl;
+    return v.begin()-1;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,8 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-typedef struct
-{
-    string name;
-    void(*pf)(void *ptr);
-}OOP;
-
-void sayname(void* ptr)
-{
-    OOP* obj = (OOP*) ptr;
-    cout << '\t' << obj->name << endl;
-}
-
-void changetoupper(string & s)
-{
-    for(auto & iter : s)
-    {
-        iter -= ('a' - 'A');
-    }
-}
+#include "binsearch.hpp"
 
 int main(void)
 {
-    OOP O1;
-    O1.name = "Mohammed" ;
-    O1.pf = &sayname;
-    O1.pf(&O1);
-    string rand = "molmwjknknknkidqodjn";
-    changetoupper(rand);
-    cout << rand << endl;
+    vector<int> x = {1,2,3,4,6};
+    cout<< *binsearch<int>(x , x.begin() , x.end() , 6) << endl;
     return 0;
 }
