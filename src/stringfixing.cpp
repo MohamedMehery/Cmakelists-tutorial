f15abd22f895620ddd28fca12474fd730370243c
Mohamed Mehery <es-mohamed.abdelnasser1415@alexu.edu.eg>
2022-08-13 12:44:44 +0200

    gmock test for string fixing

--- src/stringfixing.cpp
+++ src/stringfixing.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 using namespace ::testing;
 
@@ -9,53 +10,97 @@ using namespace ::testing;
  *                            return "FeedTheDogAndWaterThePlant"
  * @return void 
  */
-int ischar(char c)
-{
-    if ( c >= 'A' && c <= 'Z')
-    {
-        return 1;
-    }
-    else if( c >= 'a' && c <= 'z' )
-    {
-        return 1;
-    }
-    else
-        return 0;
-}
+class string_man{
+    public:
 
-
-void stringfix(char * str)
-{
-    int j = 0;
-    str[0] -= (int)('a' - 'A');
-    for(int i = 0 ; str[i] != '\0' ; i++)
+    virtual int ischar(char c)
     {
-        if(ischar(str[i]))
+        if ( c >= 'A' && c <= 'Z')
         {
-            str[j++] = str[i];
+            return 1;
+        }
+        else if( c >= 'a' && c <= 'z' )
+        {
+            return 1;
         }
         else
-        {
-            str[i+1] -= (int)('a' - 'A');
-        }
+            return 0;
     }
-    str[j] = 0;
 
-    for(int i = 0 ; str[i] != '\0' ; i++)
+
+    virtual void stringfix(char * str)
     {
-        if(!((i+1)%3))
+        int j = 0;
+        str[0] -= (int)('a' - 'A');
+        for(int i = 0 ; str[i] != '\0' ; i++)
         {
-            str[i] = 'X';
+            if(ischar(str[i]))
+            {
+                str[j++] = str[i];
+            }
+            else
+            {
+                str[i+1] -= (int)('a' - 'A');
+            }
         }
+        str[j] = 0;
+
+        for(int i = 0 ; str[i] != '\0' ; i++)
+        {
+            if(!((i+1)%3))
+            {
+                str[i] = 'X';
+            }
+        }
+
+        printf("\t%s\n",str);
     }
 
-    printf("\t%s\n",str);
-}
+};
 
-TEST(teststringfixing , teststring)
+class classtest : public ::testing::Test
+{
+    public:
+        string_man *  stm;
+        void SetUp()
+        {
+            stm = new (string_man);
+
+        }
+        void TearDown()
+        {
+            delete stm;
+        }
+};
+
+class Mockstr : public string_man
+{
+    public:
+        MOCK_METHOD1( stringfix , void(char * str) );
+};
+
+
+TEST_F(classtest , teststring)
 {
     char str[50]="feed-the dog/and@water the plant"; 
-    stringfix(str);
+    stm->stringfix(str);
+    EXPECT_STREQ(str, "FeXdTXeDXgAXdWXteXThXPlXnt" );
+}
+
+TEST(calltest , calltest)
+{
+    //Arrang
+    string_man stm;
+    Mockstr ms;
+    char str [] = "feed-the dog/and@water the plant"; 
+    EXPECT_CALL(ms , stringfix(str))
+    .Times(::testing::AtLeast(1))
+    .WillRepeatedly( ::testing::Invoke( &stm , &string_man::stringfix));
+
+    //ACT
+    ms.stringfix(str);
+
+    //Assert
     EXPECT_STREQ(str, "FeXdTXeDXgAXdWXteXThXPlXnt" );
 }
 
@@ -64,7 +109,5 @@ int main(int argc , char** argv)
     ::testing::InitGoogleTest(&argc , argv);
     // scanf("%s" , str);
     // printf("You entered \"%s\" \n" , str);
-
-
     return RUN_ALL_TESTS();
 }
